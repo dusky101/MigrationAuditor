6dd9dd6179c177f8da9f595936c3cfacbc2f5d91
Marc Oliff <marc.oliff@outlook.com>
2026-01-02 02:55:23 +0000

    Initial Commit

---
+++ MigrationAuditor/MigrationAuditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MigrationAuditorApp.swift
+//  MigrationAuditor
+//
+//  Created by Marc on 02/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MigrationAuditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
